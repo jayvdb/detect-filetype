0c53605e9d08b8182a309c8db37ea947e8093534
John Vandenberg <jayvdb@gmail.com>
2024-11-27 16:24:33 +0800

    Add TIFF

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[non_exhaustive]
 #[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
 pub enum FileType {
     // -- Images --
@@ -5,6 +6,8 @@ pub enum FileType {
     Jpeg,
     Png,
     Bmp,
+    Tiff,
+    BigTiff,
 
     // -- Compression/Archives --
     Zip,
@@ -23,6 +26,8 @@ impl FileType {
             FileType::Jpeg => "jpg",
             FileType::Png => "png",
             FileType::Bmp => "bmp",
+            FileType::Tiff => "tif",
+            FileType::BigTiff => "tif",
             FileType::Zip => "zip",
             FileType::Bzip2 => "bz2",
             FileType::Tar => "tar",
@@ -99,6 +104,10 @@ const MAGIC_MAP: &[(Magic, FileType)] = &[
         FileType::Png,
     ),
     (Magic::starts_with(b"BM"), FileType::Bmp),
+    (Magic::starts_with(b"II\x2a\x00"), FileType::Tiff), // Little Endian
+    (Magic::starts_with(b"MM\x00\x2a"), FileType::Tiff), // Big Endian
+    (Magic::starts_with(b"II\x2b\x00"), FileType::BigTiff), // Little Endian
+    (Magic::starts_with(b"MM\x00\x2b"), FileType::BigTiff), // Big Endian
     (Magic::starts_with(b"BZh"), FileType::Bzip2),
     (Magic::starts_with(&[0x50, 0x4b, 0x03, 0x04]), FileType::Zip),
     (Magic::starts_with(&[0x50, 0x4b, 0x05, 0x06]), FileType::Zip),
@@ -155,6 +164,8 @@ mod tests {
     file_test!(jpg, Jpeg);
     file_test!(png, Png);
     file_test!(bmp, Bmp);
+    file_test!(tif, Tiff);
+    file_test!(bif, BigTiff);
     file_test!(zip, Zip);
     file_test!(bz2, Bzip2);
     file_test!(tar, Tar);
